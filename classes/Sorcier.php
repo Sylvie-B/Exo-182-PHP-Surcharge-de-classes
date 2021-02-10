e5675810560854b4edfcbf4d98d8f57e0458635e
Sylvie-B <unsolo@hotmail.fr>
2021-02-10 15:43:39 +0100

    create sorcier extends Personnage

---
+++ classes/Sorcier.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Sorcier extends Personnage
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // def des propriétés de sorcier
+        $this->x = 125;
+        $this->y = 125;
+    }
+}
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
+
 // J'inclue mes deux classes ici, faites bien attention à l'ordre d'inclusion, comme la classe dragon hérite de la classe
 // personnage, je dois inclure la classe personnage avant d'inclure la classe dragon
 // TODO attention, certaines erreurs volontaires ont pu être glissées, a vous de vérifier ! Ce message en lui même est peut être un piège :-)
 require "classes/Personnage.php";
 require "classes/Dragon.php";
 require "classes/Princesse.php";
+require "classes/Sorcier.php";
 
 // Creation d'une instance de la classe personnage
 $humain = new Personnage();
@@ -15,6 +17,10 @@ $humain->setNom("Votre nom");
 $dragon = new Dragon();
 $dragon->setNom("");
 
+$princess = new Princesse();
+$sorcer = new Sorcier();
 
+$princess->setNom("Leia");
+echo $princess->getNom();
 
-
+echo $sorcer->setNom("Gandalf");
